3d34b5660fed41f2d6afadab87742323c61a1d87
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-05-30 20:01:30 +0700

    File code

---
+++ lab5_dangnhpc03033/src/main/java/com/example/demo/Lab5Dangnhpc03033Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5Dangnhpc03033Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5Dangnhpc03033Application.class, args);
+	}
+
+}
